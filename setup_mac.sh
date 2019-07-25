ff48ba89b6eaf63d4537af5fb6083e6ab3287a60
Keivaun Waugh <git@keivaun.com>
2019-07-25 12:46:44 -0500

    Add fzf. refactor some installations.

--- setup_mac.sh
+++ setup_mac.sh
@@ -29,8 +29,10 @@ chsh -s $(which zsh)
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew tap sambadevi/powerlevel9k
 # COMMAND LINE APPS
-brew install kryptco/tap/kr stow python2 python3 speedtest-cli macvim neovim
+brew install kryptco/tap/kr stow python2 python3 speedtest-cli macvim neovim \
+    fzf powerlevel9k zsh-syntax-highlighting
 
 # pair krypton
 kr pair
@@ -55,20 +57,17 @@ git config --global user.email "git@keivaun.com"
 
 # NEOVIM SETUP
 pip3 install neovim
-pip2 install neovim
+pip2 install neovim powerline-status
 
 # CASKS
-brew cask install lastpass alfred dropbox spectacle nightowl protonmail-bridge
-brew cask install signal appcleaner spotify dash iterm2 firefox
+brew cask install lastpass alfred dropbox spectacle nightowl protonmail-bridge \
+    signal appcleaner spotify dash iterm2 firefox
 
 # APPSTORE APPS
 # pages, numbers, keynote, xcode, any.do, caffeinated
 
 # POWERLEVEL9K
 # configure according to: https://github.com/bhilburn/powerlevel9k/wiki/Install-Instructions#step-1-install-powerlevel9k
-brew tap sambadevi/powerlevel9k
-brew install powerlevel9k zsh-syntax-highlighting
-pip install --user powerline-status
 # Download/install powerline font. Set as iTerm2 font.
 # https://github.com/powerline/fonts/blob/master/Meslo%20Dotted/Meslo%20LG%20M%20DZ%20Regular%20for%20Powerline.ttf
 # iTerm2 Theme
